2aeed726a418e8c0e0b7455a114f4cb114af8bca
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-08-25 09:30:07 +0100

    Add more gamemodes

--- assets/js/division.js
+++ assets/js/division.js
@@ -1,10 +1,11 @@
 // ===========variables===========
 var n1 = 0
 var n2 = 0
+var n3 = 0
 var question_tally = 0
 var tries_tally = 0
 var type = ""
-var num_max = 20
+var num_max = 5
 
 // ===========modules===========
 function RandomInt(min,max) {
@@ -34,19 +35,19 @@ async function opacity_to_0(id){
     }
 }
 
-async function start_game(){
+async function start_division_game(){
     await opacity_to_0("main")
     var doc = `<p id="result" class="result">Please answer the question</p>
     <p id="question" class="question"></p>
     <input class="answer" id="answer" type="number" placeholder="Answer" max="9999" min="0">
     <br>
     <button onclick="submit()">Check</button>
-    <button onclick="new_question()">Skip</button>
+    <button onclick="new_division_question()">Skip</button>
     <button onclick="menu()">menu</button>
     <p id="tries_tally" style="margin-bottom: 0px;">Tries:</p>
     <p id="correct_tally" style="margin-top: 0px;">Questions Correct:</p>`
     document.getElementById("main").innerHTML = doc
-    new_question()
+    new_division_question()
     var enter = document.getElementById("answer");
     enter.addEventListener("keyup", function(event) {
         if (event.key === "Enter") {
@@ -59,17 +60,19 @@ async function start_game(){
 }
 
 // ===========question check===========
-async function add_check(){
+async function division_check(){
     var opacity = 100
-    if(document.getElementById("answer").value == n1+n2){
+    if(document.getElementById("answer").value == n3/n1){
         await opacity_to_0("result")
         document.getElementById("result").innerHTML = "Correct"
         document.getElementById("result").style.color = "green"
-        new_question()
+        new_division_question()
         await opacity_to_100("result")
         add_question_tally()
         add_tries_tally()
-        num_max = num_max + 2
+        if (num_max < 12){
+            num_max = num_max + 2
+        }
         document.getElementById("answer").style.color = "black"
         document.getElementById("answer").value = ""
     } else {
@@ -85,38 +88,9 @@ async function add_check(){
     }
     clear()
 }
-async function subtract_check(){
-    var opacity = 100
-    if(document.getElementById("answer").value == n1-n2){
-        await opacity_to_0("result")
-        new_question()
-        document.getElementById("result").innerHTML = "Correct"
-        document.getElementById("result").style.color = "green"
-        await opacity_to_100("result")
-        add_question_tally()
-        add_tries_tally()
-        num_max = num_max + 2
-        document.getElementById("answer").style.color = "black"
-        document.getElementById("answer").value = ""
-    } else {
-        add_tries_tally()
-        await opacity_to_0("result")
-        document.getElementById("result").innerHTML = "Incorrect"
-        document.getElementById("result").style.color = "red"
-        document.getElementById("answer").style.color = "red"
-        await opacity_to_100("result")
-        if (num_max > 2){
-            num_max = num_max - 2
-        }
-    }
-clear()
-}
+
 async function submit(){
-    if(type == "add"){
-        add_check()
-    } else {
-        subtract_check()
-    }
+    division_check()
 }
 
 // ===========messages and question===========
@@ -132,23 +106,13 @@ async function clear(){
 
 
 
-async function new_question(){
+async function new_division_question(){
     await opacity_to_0("question")
-    type = "add"
-    if(RandomInt(0,1) == 1){
-        type = "subtract"
-    }
-    if(type == "add"){
-        n1 = RandomInt(1,num_max)
-        n2 = RandomInt(1,num_max)
-        document.getElementById("question").innerHTML = n1+" + "+n2+"="
 
-    }
-    if(type == "subtract"){
-        n1 = RandomInt(num_max+1,num_max+3)
-        n2 = RandomInt(1,num_max+1)
-        document.getElementById("question").innerHTML = n1+" - "+n2+"="
-    }
+    n1 = RandomInt(1,num_max)
+    n2 = RandomInt(1,num_max)
+    n3 = n1*n2
+    document.getElementById("question").innerHTML = n3+" ÷ "+n1+"="
     await opacity_to_100("question")
 }
 
--- assets/js/multiplication.js
+++ assets/js/multiplication.js
@@ -4,7 +4,7 @@ var n2 = 0
 var question_tally = 0
 var tries_tally = 0
 var type = ""
-var num_max = 20
+var num_max = 5
 
 // ===========modules===========
 function RandomInt(min,max) {
@@ -34,19 +34,19 @@ async function opacity_to_0(id){
     }
 }
 
-async function start_game(){
+async function start_multiplication_game(){
     await opacity_to_0("main")
     var doc = `<p id="result" class="result">Please answer the question</p>
     <p id="question" class="question"></p>
     <input class="answer" id="answer" type="number" placeholder="Answer" max="9999" min="0">
     <br>
     <button onclick="submit()">Check</button>
-    <button onclick="new_question()">Skip</button>
+    <button onclick="new_multiplication_question()">Skip</button>
     <button onclick="menu()">menu</button>
     <p id="tries_tally" style="margin-bottom: 0px;">Tries:</p>
     <p id="correct_tally" style="margin-top: 0px;">Questions Correct:</p>`
     document.getElementById("main").innerHTML = doc
-    new_question()
+    new_multiplication_question()
     var enter = document.getElementById("answer");
     enter.addEventListener("keyup", function(event) {
         if (event.key === "Enter") {
@@ -59,17 +59,19 @@ async function start_game(){
 }
 
 // ===========question check===========
-async function add_check(){
+async function multiply_check(){
     var opacity = 100
-    if(document.getElementById("answer").value == n1+n2){
+    if(document.getElementById("answer").value == n1*n2){
         await opacity_to_0("result")
         document.getElementById("result").innerHTML = "Correct"
         document.getElementById("result").style.color = "green"
-        new_question()
+        new_multiplication_question()
         await opacity_to_100("result")
         add_question_tally()
         add_tries_tally()
-        num_max = num_max + 2
+        if (num_max < 12){
+            num_max = num_max + 2
+        }
         document.getElementById("answer").style.color = "black"
         document.getElementById("answer").value = ""
     } else {
@@ -85,38 +87,9 @@ async function add_check(){
     }
     clear()
 }
-async function subtract_check(){
-    var opacity = 100
-    if(document.getElementById("answer").value == n1-n2){
-        await opacity_to_0("result")
-        new_question()
-        document.getElementById("result").innerHTML = "Correct"
-        document.getElementById("result").style.color = "green"
-        await opacity_to_100("result")
-        add_question_tally()
-        add_tries_tally()
-        num_max = num_max + 2
-        document.getElementById("answer").style.color = "black"
-        document.getElementById("answer").value = ""
-    } else {
-        add_tries_tally()
-        await opacity_to_0("result")
-        document.getElementById("result").innerHTML = "Incorrect"
-        document.getElementById("result").style.color = "red"
-        document.getElementById("answer").style.color = "red"
-        await opacity_to_100("result")
-        if (num_max > 2){
-            num_max = num_max - 2
-        }
-    }
-clear()
-}
+
 async function submit(){
-    if(type == "add"){
-        add_check()
-    } else {
-        subtract_check()
-    }
+    multiply_check()
 }
 
 // ===========messages and question===========
@@ -132,23 +105,12 @@ async function clear(){
 
 
 
-async function new_question(){
+async function new_multiplication_question(){
     await opacity_to_0("question")
-    type = "add"
-    if(RandomInt(0,1) == 1){
-        type = "subtract"
-    }
-    if(type == "add"){
-        n1 = RandomInt(1,num_max)
-        n2 = RandomInt(1,num_max)
-        document.getElementById("question").innerHTML = n1+" + "+n2+"="
 
-    }
-    if(type == "subtract"){
-        n1 = RandomInt(num_max+1,num_max+3)
-        n2 = RandomInt(1,num_max+1)
-        document.getElementById("question").innerHTML = n1+" - "+n2+"="
-    }
+    n1 = RandomInt(1,num_max)
+    n2 = RandomInt(1,num_max)
+    document.getElementById("question").innerHTML = n1+" × "+n2+"="
     await opacity_to_100("question")
 }
 
